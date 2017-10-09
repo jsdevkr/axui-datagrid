d4a8dde19a1f4da1efab2fad0c7c1eb848f15ace
Thomas Jang <tom@axisj.com>
2017-10-09 16:43:28 +0900

    GridBody Component ~ 3

--- src/component/GridBody.js
+++ src/component/GridBody.js
@@ -16,70 +16,104 @@ const GridBody = ({
                     bodyRowData
                   }) => {
 
+  const printBody = function (_panelName, _style) {
+    return (
+      <div data-panel={_panelName} style={_style}>
+        <table></table>
+      </div>
+    )
+  };
 
-  let topAsideBodyPanelStyle = {};
-  let bottomAsideBodyPanelStyle = {};
+  let topAsideBodyPanelStyle = {
+    left: 0,
+    width: styles.asidePanelWidth,
+    top: 0,
+    height: styles.frozenRowHeight
+  };
+  let bottomAsideBodyPanelStyle = {
+    left: 0,
+    width: styles.asidePanelWidth,
+    top: styles.bodyHeight - styles.footSumHeight,
+    height: styles.footSumHeight
+  };
   let asideBodyPanelStyle = {
     left: 0,
     width: styles.asidePanelWidth,
-    height: styles.headerHeight
+    top: styles.frozenRowHeight,
+    height: styles.bodyHeight - styles.frozenRowHeight - styles.footSumHeight
   };
-  let topLeftBodyPanelStyle = {};
-  let bottomLeftBodyPanelStyle = {};
+
+  let topLeftBodyPanelStyle = {
+    left: styles.asidePanelWidth,
+    width: styles.frozenPanelWidth,
+    top: 0,
+    height: styles.frozenRowHeight
+  };
+  let bottomLeftBodyPanelStyle = {
+    left: styles.asidePanelWidth,
+    width: styles.frozenPanelWidth,
+    top: styles.bodyHeight - styles.footSumHeight,
+    height: styles.footSumHeight
+  };
   let leftBodyPanelStyle = {
     left: styles.asidePanelWidth,
     width: styles.frozenPanelWidth,
-    height: styles.headerHeight
+    top: styles.frozenRowHeight,
+    height: styles.bodyHeight - styles.frozenRowHeight - styles.footSumHeight
   };
-  let topBodyPanelStyle = {};
-  let bottomBodyPanelStyle = {};
+
+  let topBodyPanelStyle = {
+    left: styles.frozenPanelWidth + styles.asidePanelWidth,
+    width: styles.CTInnerWidth - styles.asidePanelWidth - styles.frozenPanelWidth - styles.rightPanelWidth,
+    top: 0,
+    height: styles.frozenRowHeight
+  };
+  let bottomBodyPanelStyle = {
+    left: styles.frozenPanelWidth + styles.asidePanelWidth,
+    width: styles.CTInnerWidth - styles.asidePanelWidth - styles.frozenPanelWidth - styles.rightPanelWidth,
+    top: styles.bodyHeight - styles.footSumHeight,
+    height: styles.footSumHeight
+  };
   let bodyPanelStyle = {
     left: styles.frozenPanelWidth + styles.asidePanelWidth,
     width: styles.CTInnerWidth - styles.asidePanelWidth - styles.frozenPanelWidth - styles.rightPanelWidth,
-    height: styles.headerHeight
+    top: styles.frozenRowHeight,
+    height: styles.bodyHeight - styles.frozenRowHeight - styles.footSumHeight
   };
 
   return (
-    <div className={classNames(sass.gridBody)} style={{}}>
-      <div data-panel="top-aside-body" style={topAsideBodyPanelStyle}>
-
-      </div>
-      <div data-panel="top-left-body" style={topLeftBodyPanelStyle}>
-
-      </div>
-      <div data-panel="top-body-scroll-container" style={topBodyPanelStyle}>
-        <div data-panel="top-body-scroll">
-
+    <div className={classNames(sass.gridBody)} style={{height: styles.bodyHeight}}>
+      {(styles.asidePanelWidth > 0 && styles.frozenRowHeight > 0) ? printBody("top-aside-body", topAsideBodyPanelStyle) : <div data-panel="top-aside-body" style={{display: "none"}} />}
+      {(styles.frozenPanelWidth > 0 && styles.frozenRowHeight > 0) ? printBody("top-left-body", topLeftBodyPanelStyle) : <div data-panel="top-left-body" style={{display: "none"}} />}
+      {(styles.frozenRowHeight > 0) ? (
+        <div data-panel="top-body-scroll-container" style={topBodyPanelStyle}>
+          {printBody("top-body-scroll", {height: topBodyPanelStyle.height})}
         </div>
-      </div>
+      ) : <div data-panel="top-body-scroll-container" style={{display:"none"}} />}
 
-      <div data-panel="aside-body-scroll-container" style={asideBodyPanelStyle}>
-        <div data-panel="aside-body-scroll">
-
+      {(styles.asidePanelWidth > 0) ? (
+        <div data-panel="aside-body-scroll-container" style={asideBodyPanelStyle}>
+          {printBody("aside-body-scroll", {height: asideBodyPanelStyle.height})}
         </div>
-      </div>
-      <div data-panel="left-body-scroll-container" style={leftBodyPanelStyle}>
-        <div data-panel="left-body-scroll">
+      ) : <div data-panel="aside-body-scroll-container" style={{display:"none"}} />}
 
+      {(styles.frozenPanelWidth > 0) ? (
+        <div data-panel="left-body-scroll-container" style={leftBodyPanelStyle}>
+          {printBody("left-body-scroll", {height: leftBodyPanelStyle.height})}
         </div>
-      </div>
+      ) : <div data-panel="left-body-scroll-container" style={{display:"none"}} />}
+
       <div data-panel="body-scroll-container" style={bodyPanelStyle}>
-        <div data-panel="body-scroll">
+        {printBody("body-scroll", {height: bodyPanelStyle.height})}
+      </div>
 
+      {(styles.asidePanelWidth > 0 && styles.footSumHeight > 0) ? printBody("bottom-aside-body", bottomAsideBodyPanelStyle)  : <div data-panel="bottom-aside-body" style={{display:"none"}} />}
+      {(styles.frozenPanelWidth > 0 && styles.footSumHeight > 0) ? printBody("bottom-left-body", bottomLeftBodyPanelStyle)  : <div data-panel="bottom-left-body" style={{display:"none"}} />}
+      {(styles.footSumHeight > 0) ? (
+        <div data-panel="bottom-body-scroll-container" style={bottomBodyPanelStyle}>
+          {printBody("bottom-body-scroll", {height: bottomBodyPanelStyle.height})}
         </div>
-      </div>
-
-      <div data-panel="bottom-aside-body" style={bottomAsideBodyPanelStyle}>
-
-      </div>
-      <div data-panel="bottom-left-body" style={bottomLeftBodyPanelStyle}>
-
-      </div>
-      <div data-panel="bottom-body-scroll-container" style={bottomBodyPanelStyle}>
-        <div data-panel="bottom-body-scroll">
-
-        </div>
-      </div>
+      ) : <div data-panel="bottom-body-scroll-container" style={{display:"none"}} />}
     </div>
   );
 };
