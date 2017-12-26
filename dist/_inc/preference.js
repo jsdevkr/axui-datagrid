3083912d1ec0309d2f3910cb3279de7c5e8cfdd4
Thomas Jang <tom@axisj.com>
2017-12-26 15:32:59 +0900

    update .gitignore

--- dist/_inc/preference.js
+++
@@ -1,7 +0,0 @@
-const supportTouch = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-
-export const mouseEventNames = {
-  "mousedown": supportTouch ? "touchstart" : "mousedown",
-  "mousemove": supportTouch ? "touchmove" : "mousemove",
-  "mouseup": supportTouch ? "touchend" : "mouseup"
-};
