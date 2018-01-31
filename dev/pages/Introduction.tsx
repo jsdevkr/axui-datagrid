b5a0e948ca04247f6de41967dd6bce701d84ed8a
Thomas Jang <tom@axisj.com>
2018-01-31 15:38:37 +0900

    add axui logo

--- dev/pages/Introduction.tsx
+++ dev/pages/Introduction.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Header, Segment, List } from 'semantic-ui-react';
+import { Container, Header, Image, List, Segment } from 'semantic-ui-react';
+import axuiLogo from '../assets/axui-logo.png';
 
-export const Introduction = (props) => (
+export const Introduction = ( props ) => (
   <Container>
+
+    <Segment textAlign={'center'} basic padded>
+      <Image src={axuiLogo} size='small' centered />
+      <h1>AXUI datagrid</h1>
+    </Segment>
+
     <Segment basic padded>
       <h1>Introduction</h1>
     </Segment>
@@ -11,7 +18,7 @@ export const Introduction = (props) => (
     <Segment basic padded>
       <Header as='h2' dividing>Background</Header>
       <p>
-        AXUI datagrid is a 'datagrid' component created with 'React + ES6 + TS' code for chrome application development.<br/>
+        AXUI datagrid is a 'datagrid' component created with 'React + ES6 + TS' code for chrome application development.<br />
         A 'datagrid' is a component that represents data as a spreadsheet. The DataGrid component has many different functions depending on the user's various needs.
         AXUI datagrid believes that expressing large amounts of data at high speed and supporting developers fully support 'React + ES6 + TS' development environment is the most important value.
       </p>
@@ -28,21 +35,14 @@ export const Introduction = (props) => (
     <Segment basic padded>
       <Header as='h2' dividing>React + ES6 + TS</Header>
       <p>
-        The AXUI datagrid was developed using the React framework. So do not use jQuery.<br/>
+        The AXUI datagrid was developed using the React framework. So do not use jQuery.<br />
         In order to adapt to rapidly changing JS development environment, we use 'babel' to create code based on ES6 + TS, and use Typescript for systematic development.
         If you're trying to build a web application with React, and you want to make all the code you use in your project complete ES6 + TS, the AXUI datagrid will be a good choice.
       </p>
     </Segment>
 
     <Segment basic padded>
-      <Header as='h2' dividing>Expressing large amounts of data</Header>
-      <p>
-
-      </p>
-    </Segment>
-
-    <Segment basic padded>
-      <Header as='h2' dividing>Formatting data</Header>
+      <Header as='h2' dividing>Feature</Header>
       <p>
 
       </p>
--- dev/typings/global.d.ts
+++ dev/typings/global.d.ts
@@ -2,6 +2,14 @@ declare module '*.json' {
   const value: any;
   export default value;
 }
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+declare module '*.jpg' {
+  const value: any;
+  export default value;
+}
 
 interface iSideNavProps {
   style: any;
