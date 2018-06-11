97b71c64731a5a94117dc65591f5fda4582c21e4
Thomas Jang <tom@axisj.com>
2018-06-11 14:54:05 +0900

    refactoring : add to capitalize component

---
+++ src/axui-datagrid/components/DataGridBody.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { typeStore } from '../stores';
+
+interface IProps {}
+interface IState {}
+
+class DatagridBody extends React.Component<IProps, IState> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default DatagridBody;
