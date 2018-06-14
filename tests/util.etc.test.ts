d2f04731e4df1410e8895bb723bebe4db044f841
Thomas Jang <tom@axisj.com>
2018-06-14 18:48:40 +0900

    refactoring : add util getPathValue

--- tests/util.etc.test.ts
+++ tests/util.etc.test.ts
@@ -1,10 +1,11 @@
 import { types } from 'axui-datagrid/stores';
 import { getPathValue } from 'axui-datagrid/utils';
 
-test('test : getPathValue - 1depth', () => {
-  const tgObj = { a: 1, b: { c: 2 } };
+test('test : getPathValue', () => {
+  const tgObj = { a: 1, b: { c: 2 }, arr: [1, 2, 3, 4] };
 
   expect(getPathValue(tgObj, ['a'])).toBe(1);
   expect(getPathValue(tgObj, ['b', 'c'])).toBe(2);
+  expect(getPathValue(tgObj, ['arr', 3])).toBe(4);
   expect(getPathValue(tgObj, ['a', 'c'], 3)).toBe(3);
 });
