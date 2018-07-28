7d8bb594e7920405bafda8a2af43ae42eb9cce36
Thomas Jang <tom@axisj.com>
2018-07-28 23:26:50 +0900

    update readme

--- src/axui-datagrid/components/DataGridBodyCell.tsx
+++ src/axui-datagrid/components/DataGridBodyCell.tsx
@@ -197,7 +197,7 @@ class DataGridBodyCell extends React.Component<IProps, IState> {
             onKeyUp={(e: any) => {
               this.onEventInput(EventNames.KEYUP, e);
             }}
-            data-inline-edit
+            data-inline-editz
             defaultValue={value}
           />
         </td>
