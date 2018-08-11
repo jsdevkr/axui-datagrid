384dcccec26e540b090dffda82e8f07dcc2b7f44
Thomas Jang <tom@axisj.com>
2018-08-11 16:00:09 +0900

    update package.json

--- src/axui-datagrid/hoc/connectStore.tsx
+++ src/axui-datagrid/hoc/connectStore.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { DataGridStore, IDataGridStore } from '../providers';
 
-// Diff / Omit taken from https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-311923766
-export type Diff<T extends string, U extends string> = ({ [P in T]: P } &
-  { [P in U]: never } & { [x: string]: never })[T];
-export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+// Omit taken from https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-311923766
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 export type IConnectStore = IDataGridStore;
 
