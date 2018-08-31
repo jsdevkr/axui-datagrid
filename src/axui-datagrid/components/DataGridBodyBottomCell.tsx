fd6d7fabdff4db05818a1bc293ef33cf24d15de4
Thomas <tom@axisj.com>
2018-08-31 11:38:58 +0900

    convert to SFC

--- src/axui-datagrid/components/DataGridBodyBottomCell.tsx
+++ src/axui-datagrid/components/DataGridBodyBottomCell.tsx
@@ -1,101 +1,48 @@
 import * as React from 'react';
-import { intfs, types, DispatchTypes, EventNames, KeyCodes } from '../stores';
+import { intfs, types } from '../stores';
 import { connectStore } from '../hoc';
 import { IDataGridStore } from '../providers';
-import { classNames as CX, isFunction, getNode } from '../utils';
+import { classNames as CX, isFunction } from '../utils';
 
 interface IProps extends IDataGridStore {
   ci: number;
   col?: types.DataGridCol;
   value?: any;
 }
-interface IState {}
 
-class DataGridBodyBottomCell extends React.Component<IProps, IState> {
-  state = {};
+const CellLabel: React.SFC<{
+  lineHeight: number;
+  col: types.DataGridCol;
+  list: any[];
+  predefinedFormatter: types.DataGridFormatter;
+  predefinedCollector: types.DataGridCollector;
+}> = props => {
+  const {
+    col,
+    list: data,
+    lineHeight,
+    predefinedFormatter,
+    predefinedCollector,
+  } = props;
+  const { key, label = '', columnAttr = '', collector, formatter } = col;
+  let collectorData: intfs.IDataGridCollectorData = {
+    data,
+    key,
+  };
+  let formatterData: intfs.IDataGridFormatterData = {
+    data,
+    key,
+    value: '',
+  };
 
-  render() {
-    const {
-      filteredList = [],
-      focusedRow,
-      focusedCol,
-      selectionRows = [],
-      selectionCols = [],
-      col = {},
-      ci,
-      value,
-      options = {},
-      predefinedFormatter = {},
-      predefinedCollector = {},
-    } = this.props;
-
-    const { body: optionsBody = {} } = options;
-    const {
-      columnHeight = 0,
-      columnPadding = 0,
-      columnBorderWidth = 0,
-      align: bodyAlign = 'left',
-    } = optionsBody;
-    const { rowSpan: colRowSpan = 0, colIndex: colColIndex = 0 } = col;
-
-    let cellHeight = columnHeight * colRowSpan;
-    let tdClassNames: { [key: string]: any } = {
-      ['axui-datagrid-line-number']: col.columnAttr === 'lineNumber',
-      ['axui-datagrid-row-selector']: col.columnAttr === 'rowSelector',
-    };
-
-    const lineHeight: number =
-      columnHeight - columnPadding * 2 - columnBorderWidth;
-    const colAlign = col.align || bodyAlign || '';
-
-    let label: any;
-
-    const getLabel = function() {
-      let collectorData: intfs.IDataGridCollectorData = {
-        data: filteredList,
-        key: col.key,
-      };
-      let formatterData: intfs.IDataGridFormatterData = {
-        data: filteredList,
-        key: col.key,
-        value: '',
-      };
-
-      let labelValue: string;
-
-      if (
-        typeof col.collector === 'string' &&
-        col.collector in predefinedCollector
-      ) {
-        labelValue = predefinedCollector[col.collector](collectorData);
-      } else if (isFunction(col.collector)) {
-        labelValue = (col.collector as types.collectorFunction)(collectorData);
-      } else {
-        labelValue = col.label || '';
-      }
-
-      // collector로 구한 값을 formatter의 value로 사용
-      formatterData.value = labelValue;
-
-      if (
-        typeof col.formatter === 'string' &&
-        col.formatter in predefinedFormatter
-      ) {
-        labelValue = predefinedFormatter[col.formatter](formatterData);
-      } else if (isFunction(col.formatter)) {
-        labelValue = (col.formatter as types.formatterFunction)(formatterData);
-      }
-
-      return labelValue;
-    };
-
-    if (col.key === '_line_number_') {
-      label = '';
-    } else if (col.key === '_row_selector_') {
-      label = (
+  switch (key) {
+    case '_line_number_':
+      return null;
+    case '_row_selector_':
+      return (
         <div
           className="axui-datagrid-check-box"
-          data-span={col.columnAttr || ''}
+          data-span={columnAttr}
           data-checked={false}
           style={{
             maxHeight: lineHeight + 'px',
@@ -103,32 +50,88 @@ class DataGridBodyBottomCell extends React.Component<IProps, IState> {
           }}
         />
       );
-    } else {
-      label = getLabel();
-    }
+    default:
+      let labelValue: string;
+      if (typeof collector === 'string' && collector in predefinedCollector) {
+        labelValue = predefinedCollector[collector](collectorData);
+      } else if (isFunction(collector)) {
+        labelValue = (collector as types.collectorFunction)(collectorData);
+      } else {
+        labelValue = label;
+      }
 
-    return (
-      <td
-        key={ci}
-        colSpan={col.colSpan}
-        rowSpan={col.rowSpan}
-        className={CX(tdClassNames)}
-        style={{ height: cellHeight, minHeight: '1px' }}
-      >
-        <span
-          data-span={col.columnAttr || ''}
-          data-pos={col.colIndex}
-          style={{
-            height: columnHeight - columnBorderWidth + 'px',
-            lineHeight: lineHeight + 'px',
-            textAlign: colAlign as any,
-          }}
-        >
-          {label || ''}
-        </span>
-      </td>
-    );
+      // set formatterData.value by collector value
+      formatterData.value = labelValue;
+
+      if (typeof formatter === 'string' && formatter in predefinedFormatter) {
+        labelValue = predefinedFormatter[formatter](formatterData);
+      } else if (isFunction(col.formatter)) {
+        labelValue = (col.formatter as types.formatterFunction)(formatterData);
+      }
+
+      return <>{labelValue}</>;
   }
-}
+};
+
+const DataGridBodyBottomCell: React.SFC<IProps> = props => {
+  const {
+    filteredList = [],
+    col = {},
+    ci,
+    options = {},
+    predefinedFormatter = {},
+    predefinedCollector = {},
+  } = props;
+
+  const { body: optionsBody = {} } = options;
+  const {
+    columnHeight = 0,
+    columnPadding = 0,
+    columnBorderWidth = 0,
+    align: bodyAlign = 'left',
+  } = optionsBody;
+  const {
+    rowSpan: colRowSpan = 0,
+    colIndex: colColIndex = 0,
+    align: colAlign = bodyAlign || '',
+    columnAttr = '',
+    colSpan = 1,
+    rowSpan = 1,
+  } = col;
+  const tdClassNames: { [key: string]: any } = {
+    ['axui-datagrid-line-number']: columnAttr === 'lineNumber',
+    ['axui-datagrid-row-selector']: columnAttr === 'rowSelector',
+  };
+  const lineHeight: number =
+    columnHeight - columnPadding * 2 - columnBorderWidth;
+
+  return (
+    <td
+      key={ci}
+      colSpan={colSpan}
+      rowSpan={rowSpan}
+      className={CX(tdClassNames)}
+      style={{ height: columnHeight * colRowSpan, minHeight: '1px' }}
+    >
+      <span
+        data-span={columnAttr}
+        data-pos={colColIndex}
+        style={{
+          height: columnHeight - columnBorderWidth + 'px',
+          lineHeight: lineHeight + 'px',
+          textAlign: colAlign as any,
+        }}
+      >
+        <CellLabel
+          col={col}
+          list={filteredList}
+          lineHeight={lineHeight}
+          predefinedFormatter={predefinedFormatter}
+          predefinedCollector={predefinedCollector}
+        />
+      </span>
+    </td>
+  );
+};
 
 export default connectStore(DataGridBodyBottomCell);
