27616d7ffba12d39c783fe4a27e9e4e3e79668d9
Thomas Jang <tom@axisj.com>
2018-11-30 12:03:04 +0900

    update intro

--- src/pages/start/Introduction.tsx
+++ src/pages/start/Introduction.tsx
@@ -36,12 +36,13 @@ class Introduction extends React.Component<IProps, IState> {
             web application development. And I made it using 'React + ES6 + TS'.
           </p>
           <p>
-            'datagrid UI component' is a UI that shows data as a spreadsheet It
-            says. There are so many features that axui-datagrid needs to be a
-            complete `datagrid UI component`. However, the function of
+            'datagrid UI component' is a UI that shows data as a spreadsheet.
+            The 'axui-datagrid' component requires a lot of functionality to
+            become a complete 'datagrid' component. However, the function of
             outputting and controlling a large amount of data at a high speed is
-            more important than the function having all functions. We've also
-            optimized it for developers using 'React + ES6 + TS'.
+            more important than the function having all functions. So
+            `axui-datagrid` focuses on high-speed control. We've also optimized
+            it for developers using 'React + ES6 + TS'.
           </p>
         </Segment>
 
